5e88930d1e4564da90c09814738985e48a67a7b3
ammarriaz44 <ammarriaz44@gmail.com>
2024-11-11 01:40:55 +0500

    Movie endpoints are added

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
-import express from "express"
-import {ENV_VARS} from "./config/envVars.js"
+import express from "express";
 
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+
+import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const PORT = ENV_VARS.PORT
+const PORT = ENV_VARS.PORT;
 
-app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movies", movieRoutes);
 
-app.listen(PORT, ()=>{
-    console.log("Server started at http://localhost:"+PORT)
-    connectDB()
-})
+app.listen(PORT, () => {
+  console.log("Server started at http://localhost:" + PORT);
+  connectDB();
+});
